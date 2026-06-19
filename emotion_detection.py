153e76d184f7914ebd5767474433db603996cc9e
donjeta morina <donjeta.morina9@student.uni-pr.edu>
2026-06-19 12:50:13 -0400

    Complete Task 2: Create emotion detection application using Watson NLP

---
+++ emotion_detection.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    # Define the URL for the Watson NLP Emotion Predict function
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
+    # Define the headers required by the API
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    # Create the payload dictionary with the input text
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    
+    # Send the POST request to the Watson service
+    response = requests.post(url, json=myobj, headers=headers)
+    
+    # Return the text attribute of the response object
+    return response.text
